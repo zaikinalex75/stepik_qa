2a823c7734142e7b6517a9cf1d0548ddb358e2ec
Алексей Заикин <zaikinalex@inbox.ru>
2025-07-18 13:12:19 +0300

    Тест возможности входа дополнен проверками наличия форм входа и регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,11 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
     main_page.open()
-    main_page.should_contain_login_link()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
